68eed6bdcb0ef1c0a895302e98a14033597bd658
DerekTY Lin(林鼎元) <DerekTY_Lin@asus.com>
2025-02-26 17:16:15 +0800

    dev

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from langchain_openai import ChatOpenAI
 from config import LLM_PROVIDER, OPENAI_API_KEY
+from embedding import get_embedding_model
+from postgresql import get_pg_engine
+from sqlalchemy import text
+from ollama import OllamaEmbeddings
 
+
+pg_engine = get_pg_engine()
 
 def get_llm():
     if LLM_PROVIDER == "openai":
@@ -27,26 +33,61 @@ def get_llm():
         raise ValueError("不支援的 LLM 提供者，請使用 'ollama' 或 'openai'")
 
 
-user_inputs = f'''
-Christmas
-A Christian holiday signifying the birth of Jesus, Christmas is widely celebrated and enjoyed across the United States and the world. The holiday always falls on 25 December (regardless of the day of the week), and is typically accompanied by decorations, presents, and special meals.
+embedding_model = get_embedding_model(provider="ollama")
 
-Specifically, the legend behind Christmas (and the one that most children are told) is that Santa Claus, a bearded, hefty, jolly, and red-jacket-wearing old man who lives in the North Pole, spends the year crafting presents with his elves, or small, festive, excited Santa-assistants. All the children who behave throughout the year are admitted to the Good List, and will presumably receive their desired gifts on Christmas, while those who don't behave are placed on the Naughty List, and will presumably (although the matter is determined by parents) receive a lump of coal.
-
-Santa Claus is said to fly around the Christmas sky in a sled powered by his magical reindeer, or cold-resistant, mythically powered, individually named animals, delivering presents to each child's house in the process. Santa is also expected to slide through chimneys to deliver these presents (homes not equipped with chimneys might "leave the front door cracked open"), and children sometimes arrange cookies or other treats on a plate for him to enjoy.
-
-Gifts are placed underneath a Christmas tree, or a pine tree that's decorated with ornaments and/or lights and is symbolic of the holiday. Additionally, smaller gifts may be placed inside a stocking, or a sock-shaped, holiday-specific piece of fabric that's generally hung on the mantle of a fireplace (homes without fireplaces might use the wall). A Christmas tree's ornaments, or hanging, typically spherical decorations, in addition to the mentioned lights, may be accompanied by a star, or a representation of the Star of Jerusalem that the Three Apostles followed while bringing Baby Jesus gifts and honoring him, in the Bible."""),
-'''
-messages = [
-    (
-        "system",
-        "You are a helpful assistant that translates English to 繁體中文. Translate the user sentence.",
-    ),
-    ("human", user_inputs)
-]
+def get_query_embedding(embedding_model:OllamaEmbeddings):
+    user_inputs = input("請輸入詢問句子: ")
+    query_embedding = embedding_model.embed_query(user_inputs)
+    return query_embedding
 
 
-# 呼叫模型並逐步輸出
-llm = get_llm()
-for chunk in llm.stream(messages):
-    print(chunk.content, end="", flush=True)  # 逐字輸出
+def similarity_search(query_embedding, top_k=5):
+    search_query = """
+        SELECT
+            id,
+            document_name,
+            chunk_id,
+            original_text,
+            cleaned_text,
+            embedding,
+            process_datetime,
+            file_path,
+            metadata
+        FROM public."wifi_knowledge_embedding_bge"
+        WHERE embedding IS NOT NULL
+        ORDER BY embedding <-> :query_embedding
+        LIMIT 3
+    """        
+    # 使用 SQLAlchemy 的 `execute` 來執行 SQL 查詢
+    with pg_engine.connect() as conn:
+        result = conn.execute(text(search_query), {"query_embedding": query_embedding, "top_k": top_k})
+        columns = result.keys()  # 獲取欄位名稱
+        #results = result.fetchall()  # 取得查詢結果
+        results = [dict(zip(columns, row)) for row in result.fetchall()]    # 拼裝成字典 這樣有欄位資訊比較好餵進去LLM
+    return results
+
+
+query_embedding = get_query_embedding()
+results = similarity_search(query_embedding)
+print(results)
+
+
+
+
+
+
+
+
+# messages = [
+#     (
+#         "system",
+#         "You are a helpful assistant that translates English to 繁體中文. Translate the user sentence.",
+#     ),
+#     ("human", user_inputs)
+# ]
+
+
+# # 呼叫模型並逐步輸出
+# llm = get_llm()
+# for chunk in llm.stream(messages):
+#     print(chunk.content, end="", flush=True)  # 逐字輸出
